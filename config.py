ef41bd5b8625885eb7d20006be5d43dbdd94941b
runningindos <56303584+runningindos@users.noreply.github.com>
2023-12-05 20:40:28 +0000

    updated sql dialect. Hopefully this will work to resolve the issue of render suddenly not updating my site. This was fine earlier, so I'm not certain what changed???

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+app.config["SQLALCHEMY_DATABASE_URI"] = postgresql://mystorydb_user:PzW57YczDpJJH2ZNtgAv5p9SGOjWOLvK@dpg-cl9u48e2eqrc7393hn40-a/mystorydb
